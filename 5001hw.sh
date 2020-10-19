34ae193fc39f06159f17d03bc302268c65790ed1
Pengx8983 <73095292+Pengx8983@users.noreply.github.com>
2020-10-19 16:34:40 +0800

    Delete 5001hw.sh

--- 5001hw.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir DDM{1..100}
-touch DDM{1..100}/time_till_now.txT
-for i in {1..100}; do
-    	echo 'nanoseconds since 1970-01-01 00:00:00 UTC:'>>./DDM$i/time_till_now.txT
-	echo '<'`date "+%s%N"`'>'>>./DDM${i}/time_till_now.txT
-done
-
